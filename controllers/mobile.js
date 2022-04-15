1a29c543011277b05e9aae2c8f91f4708b5f8f4d
prudhvidhar18 <98135740+prudhvidhar18@users.noreply.github.com>
2022-04-15 02:26:39 -0500

    added files

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -50,3 +50,35 @@ exports.mobile_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+// for a specific Mobile. 
+exports.mobile_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Mobile.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+// Handle Mobile update form on PUT. 
+exports.mobile_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Mobile.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.mobile_brand)  
+               toUpdate.mobile_brand = req.body.mobile_brand; 
+        if(req.body.mobile_cost) toUpdate.mobile_cost = req.body.mobile_cost; 
+        if(req.body.mobile_color) toUpdate.mobile_color = req.body.mobile_color; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
