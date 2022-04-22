0186cc75eda200a78bef2557d6f6e1e592e63059
prudhvidhar18 <98135740+prudhvidhar18@users.noreply.github.com>
2022-04-22 01:15:07 -0500

    files added

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
 
 const connectionString = "mongodb+srv://admin:admin@cluster0.8mbxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose = require('mongoose');
